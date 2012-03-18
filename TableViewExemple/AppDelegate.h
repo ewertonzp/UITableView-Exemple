6e0d8e0170b68206d34c264ebdf4d0c63007e6d1
Beraldo <rbchaiben@venus.local>
2012-03-17 21:57:15 -0300

    Initial Commit

---
+++ TableViewExemple/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewExemple
+//
+//  Created by Roberto  Beraldo Chaiben on 17/03/12.
+//  Copyright (c) 2012 Master Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
